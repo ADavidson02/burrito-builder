1eddfcc932b20014c6f5d511886c543afcd4b620
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-19 12:02:20 -0700

    Add test to mock order

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,24 @@
 
+import React from 'react'
+import App from './App'
+import {render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { getOrders, addNewOrder } from '../../apiCalls'
+jest.mock('../../apiCalls')
+
+describe('App', () => {
+  it('should render orders', async () => {
+    const mockOrders =[
+    {id: 24, name: 'James', ingredients: ['steak', 'lettuce']},
+    {id: 25, name: 'Steph', ingredients: ['beans', 'jalapeno']}
+    ]
+    getOrders.mockResolvedValue({ orders: mockOrders })
+    render(
+      <App />
+    )
+    await waitFor(() => expect(screen.getByText('James')).toBeInTheDocument())
+    await waitFor(() => expect(screen.getByText('Steph')))
+  }),
+
+})
