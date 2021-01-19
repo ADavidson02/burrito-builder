14d55493a9b3ab54a73885ed7feab5281e0abc1c
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-19 11:00:19 -0700

    Add api call to add orders

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,20 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const addNewOrder = (id, name, ingredients) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json' 
+    },
+    body: JSON.stringify({
+      id: id,
+      name: name,
+      ingredients: ingredients
+    })
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error.states))
+}
